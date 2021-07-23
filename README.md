9e4e61427958cbf0e008fe622282180089d04ebe
Felicitas Pojtinger <felicitas@pojtinger.com>
2021-07-23 14:40:35 +0000

    feat: Add basic instance listing

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # gopojde
+
 Experimental Go implementation of https://github.com/pojntfx/pojde.
+
+## Overview
+
+🚧 This project is a work-in-progress! Instructions will be added as soon as it is usable. 🚧
+
+## License
+
+gopojde (c) 2021 Felicitas Pojtinger and contributors
+
+SPDX-License-Identifier: AGPL-3.0
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+)
+
+const (
+	prefix = "/pojde-"
+)
+
+func main() {
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		panic(err)
+	}
+
+	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, instance := range containers {
+		fmt.Printf("%v\n", strings.TrimPrefix(instance.Names[0], prefix))
+	}
+}
