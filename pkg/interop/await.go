ec80bc4592c6164093b448faa9f7df8776c704b2
Felicitas Pojtinger <felicitas@pojtinger.com>
2021-08-07 14:26:30 +0000

    feat: Add listing for instances

--- pkg/interop/await.go
+++ pkg/interop/await.go
@@ -2,26 +2,25 @@ package interop
 
 import (
 	"errors"
-	"fmt"
 
 	"github.com/maxence-charriere/go-app/v9/pkg/app"
 )
 
 // See https://stackoverflow.com/questions/68426700/how-to-wait-a-js-async-function-from-golang-wasm
-func Await(awaitable app.Value) ([]app.Value, error) {
-	then := make(chan []app.Value)
+func Await(awaitable app.Value) (app.Value, error) {
+	then := make(chan app.Value)
 	defer close(then)
 	thenFunc := app.FuncOf(func(this app.Value, args []app.Value) interface{} {
-		then <- args
+		then <- args[0]
 
 		return nil
 	})
 	defer thenFunc.Release()
 
-	catch := make(chan []app.Value)
+	catch := make(chan app.Value)
 	defer close(catch)
 	catchFunc := app.FuncOf(func(this app.Value, args []app.Value) interface{} {
-		catch <- args
+		catch <- args[0]
 
 		return nil
 	})
@@ -33,15 +32,6 @@ func Await(awaitable app.Value) ([]app.Value, error) {
 	case result := <-then:
 		return result, nil
 	case err := <-catch:
-		out := ""
-		for i, individualError := range err {
-			if i != 0 {
-				out += ": "
-			}
-
-			out += fmt.Sprintf("%v", individualError)
-		}
-
-		return nil, errors.New(out)
+		return nil, errors.New(err.String())
 	}
 }
